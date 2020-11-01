05d58ea32e834f29108726f0f143b2cb51711689
Thando Mini <thando.mini@kurtosys.com>
2020-11-02 00:07:34 +0200

    implemented the validator class logic within the template/view. added some styling for errors and made the form fields persist even when there are errors for the user to fix.

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <?php
+
+    require('userValidator.php');
     
     if (isset($_POST['submit'])) {
-        // validate entries
-        echo 'form submitted';
+        
+        $validation = new UserValidator($_POST);
+
+        $errors = $validation->validateForm();
+
+        // save data to database if no errors
+
+
     }
 
 ?>
@@ -20,10 +28,16 @@
         <h2>Create new user</h2>
         <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
             <label>Username:</label>
-            <input type="text" name="username">
+            <input type="text" name="username" value="<?php echo htmlspecialchars($_POST['username']) ?? '' ?>">
+            <div class="error">
+                <?php echo $errors['username'] ?? '' ?>
+            </div>
 
             <label>Email:</label>
-            <input type="text" name="email">
+            <input type="text" name="email" value="<?php echo htmlspecialchars($_POST['email']) ?? '' ?>">
+            <div class="error">
+                <?php echo $errors['email'] ?? '' ?>
+            </div>
 
             <input type="submit" value="submit" name="submit">
         </form>
