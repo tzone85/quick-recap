93a7636e8cff3728c75fe2792928b87663695b03
Thando Mini <thando.mini@kurtosys.com>
2020-11-01 16:33:14 +0200

    used access modifiers and changed one of the public properties to private and used the method to access the data instead

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
     
     class User {
         // properties and methods
-        public $username = 'thando';
-        public $email = 'thando@mail.com';
+        public $username;
+        private $email;
 
         public function __construct($username, $email) {
             $this->username = $username;
@@ -20,25 +20,8 @@
     $userOne = new User('thando', 'thando@gmail.com');
     $userTwo = new User('mncedi', 'mncedi@mail.com');
 
-    echo $userOne->username.'<br>';
-    echo $userOne->email.'<br>';
     echo $userOne->addFriend().'<br>';
-
-    $userTwo->username = 'Mncedi';
-    $userTwo->email = 'mncedi@mail.com';
-
-    echo $userTwo->username.'<br>';
-    echo $userTwo->email.'<br>';
     echo $userTwo->addFriend().'<br>';
-
-    '<br/>';
-    '<br />';
-    echo "####################################################<br/>";
-    echo "############### Some helpful methods ##################<br/>";
-    echo "####################################################<br/>";
-
-    echo "<br/> ############### get_class(objectName) ##########################<br/>";
-    echo 'The class is '.get_class($userOne);
 
     // echo "<br/><br/> ################## get_class_vars('classname') ################<br/>";
     // print_r(get_class_vars('User')).'<br/>';
